25fb3b9c4319566766c61a9c637182e03689cd9b
Talha Affan Siddique <talhaaffansiddique@gmail.com>
2023-12-13 16:11:00 +0500

    238 - Movie DB React Router

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import Form from './SearchForm'
-import Movies from './Movies'
+import React from "react";
+import Form from "./SearchForm";
+import Movies from "./Movies";
 const Home = () => {
-  return <h2>movie component</h2>
-}
+  return <h2>home page</h2>;
+};
 
-export default Home
+export default Home;
