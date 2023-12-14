cf624ad69a1dbb71209a571c2e993233c3dfa894
Talha Affan Siddique <talhaaffansiddique@gmail.com>
2023-12-14 17:44:23 +0500

    239 - Movie DB Initial Fetch

--- src/context.js
+++ src/context.js
@@ -5,7 +5,38 @@ console.log(API_ENDPOINT);
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState({ show: false, msg: "" });
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState("batman");
+
+  const fetchMovies = async (url) => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      if (data.Response === "True") {
+        setMovies(data.Search);
+        setError({ show: false, msg: "" });
+      } else {
+        setError({ Show: true });
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies(`${API_ENDPOINT}&s=${query}`);
+  }, [query]);
+
+  return (
+    <AppContext.Provider value={{ setLoading, error, movies, setQuery }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 // make sure use
 export const useGlobalContext = () => {
